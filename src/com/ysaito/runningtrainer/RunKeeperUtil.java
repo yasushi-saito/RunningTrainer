f54144a5194534dc0820a2cf4f8816492c93cc35
Yaz Saito <saito@upthere.io>
2012-09-03 22:39:39 -0700

    checkpointing.

--- src/com/ysaito/runningtrainer/RunKeeperUtil.java
+++ src/com/ysaito/runningtrainer/RunKeeperUtil.java
@@ -5,4 +5,5 @@ class RunKeeperUtil {
         // TODO: fix
         return "Sat 1 Jan 2012 00:00:00";
     }
+    // https://runkeeper.com/apps/authorize?client_id=0808ef781c68449298005c8624d3700b&response_type=code&redirect_uri=foobar://www.ysaito.com
 }
