37b899e795f3abc4bf2a242ab2f8300254b23ddd
Yaz Saito <saito@localhost>
2012-09-16 16:30:53 -0700

    checkpointing.

--- src/com/ysaito/runningtrainer/WorkoutCanvasView.java
+++ src/com/ysaito/runningtrainer/WorkoutCanvasView.java
@@ -16,14 +16,36 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
   // Below are internal versions of the classes defined in Workout.java
   // They contain all the fields in Workout.java, and a few internal things, 
   // such as the display screen locations.
-  private interface Entry {
-	  public ArrayList<Entry> getChildren();
+  private interface Element {
+	  public ArrayList<Element> getChildren();
 	  public void draw(Canvas canvas, float x, float y);
 	  public float getHeight();
 	  public RectF getLastBoundingBox();
   }
+
+  private class PlaceholderDuringMove implements Element {
+	  private float mHeight = 1.0f;
+	  private final RectF mLastBoundingBox = new RectF(-1.0f, -1.0f, -1.0f, -1.0f);
+	  public void setHeight(float height) { mHeight = height; }
+	  public ArrayList<Element> getChildren() { return null; }
+	  public void draw(Canvas canvas, float x, float y) { }
+	  public float getHeight() { return mHeight; }
+	  public RectF getLastBoundingBox() { 
+		  return mLastBoundingBox;
+	  }
+	  @Override public String toString() { 
+		  return "placeholder";
+	  }
+  }
+
+  PlaceholderDuringMove mPlaceholder = new PlaceholderDuringMove();
+  public PlaceholderDuringMove getPlaceholder(float height) {
+	  removeElement(mPlaceholder);
+	  mPlaceholder.setHeight(height);
+	  return mPlaceholder;
+  }
 	
-  private class Interval implements Entry {
+  private class Interval implements Element {
 	  // At most one of duration or distance is positive.
 	  // If duration >= 0, the interval ends at the specified time
 	  // If distance >= 0, the interval ends at the specified distance
@@ -45,8 +67,12 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 	  public Interval() {
 		  mScreenDensity = getContext().getResources().getDisplayMetrics().scaledDensity;
 	  }
+	  
+	  @Override public String toString() { 
+		  return String.format("Interval: duration=%f distance=%f pace=[%f,%f]", mDuration, mDistance, mLowTargetPace, mHighTargetPace);
+	  }
 
-	  public ArrayList<Entry> getChildren() { return null; }
+	  public ArrayList<Element> getChildren() { return null; }
 	  public float getHeight() {
 		  return 48 * mScreenDensity;
 	  }
@@ -96,9 +122,9 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 	  }
   }
   
-  private class Repeats implements Entry {
+  private class Repeats implements Element {
 	  public int mRepeats = 0;
-	  public final ArrayList<Entry> mEntries = new ArrayList<Entry>();
+	  public final ArrayList<Element> mEntries = new ArrayList<Element>();
 	  
 	  private RectF mLastBoundingBox = new RectF(-1.0f, -1.0f, -1.0f, -1.0f);
 
@@ -108,13 +134,26 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 		  mScreenDensity = getContext().getResources().getDisplayMetrics().scaledDensity;
 	  }
 
-	  public ArrayList<Entry> getChildren() { return mEntries; }
+	  @Override public String toString() { 
+		  StringBuilder b = new StringBuilder();
+		  b.append(String.format("Repeats[%d]{", mRepeats));
+		  boolean first = true;
+		  for (Element elem : mEntries) {
+			  if (!first) b.append(", ");
+			  first = false;
+			  b.append(elem.toString());
+		  }
+		  b.append("}");
+		  return b.toString();
+	  }
+
+	  public ArrayList<Element> getChildren() { return mEntries; }
 	  
 	  public RectF getLastBoundingBox() { return mLastBoundingBox; }
 	  
 	  public float getHeight() {
 		  float height = 48 * mScreenDensity;
-		  for (Entry entry : mEntries) height += entry.getHeight();
+		  for (Element entry : mEntries) height += entry.getHeight();
 		  return height;
 	  }
 	  
@@ -133,7 +172,7 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 		  mLastBoundingBox.bottom = y + 48 * mScreenDensity;
 		  
 		  y += 48 * mScreenDensity;
-		  for (Entry entry : mEntries) {
+		  for (Element entry : mEntries) {
 			  entry.draw(canvas, x + 30 * mScreenDensity, y);
 			  y += entry.getHeight();
 		  }
@@ -149,7 +188,7 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 	  return y >= bbox.top + height / 2 && y < bbox.bottom + height / 2;
   }
 	  
-  private ArrayList<Entry> mEntries = new ArrayList<Entry>();
+  private ArrayList<Element> mEntries = new ArrayList<Element>();
   
   public WorkoutCanvasView(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -193,9 +232,10 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
   //
   // onTouch processing
   //
-  private Entry mMovingEntry;
+  private Element mMovingEntry;
   private float mMoveStartX, mMoveStartY;
   private float mMoveCurrentX, mMoveCurrentY;
+  private float mMoveDeltaX, mMoveDeltaY;
   
   public boolean onTouch(View v, MotionEvent event) {
 	  final int action = event.getAction();
@@ -205,38 +245,132 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 			  RectF rect = mMovingEntry.getLastBoundingBox();
 			  mMoveStartX = event.getX();
 			  mMoveStartY = event.getY();
+			  mMoveDeltaX = event.getX() - rect.left;
+			  mMoveDeltaY = event.getY() - rect.top;
+			  
+			  replaceElement(mMovingEntry, getPlaceholder(mMovingEntry.getHeight()));
 		  }
 		  return true;
 	  }
-	  if (action == MotionEvent.ACTION_MOVE) {
+	  if (action == MotionEvent.ACTION_MOVE && mMovingEntry != null) {
 		  mMoveCurrentX = event.getX();
 		  mMoveCurrentY = event.getY();
+		  
+		  tryFindMoveDestination(mMovingEntry, event.getX(), event.getY(), null);
 		  invalidate();
 		  return true;
 	  }
 	  if (action == MotionEvent.ACTION_UP) {
+		  if (mMovingEntry != null) {
+			  replaceElement(mPlaceholder, mMovingEntry);
+		  }
 		  mMovingEntry = null;
+		  removeElement(mPlaceholder);
 		  invalidate();
 		  return true;
 	  }
 	  return true;
   }
 
-  static private Entry findEntryAtPositionRec(Entry e, float x, float y) {
+  private boolean moveToNewPosition(Element movingEntry, ArrayList<Element> list) {
+	  final int N = list.size();
+	  for (int i = 0; i < N; ++i) {
+		  if (list.get(i) == mPlaceholder) {
+			  list.set(i, movingEntry);
+			  removeElement(movingEntry);
+			  return true;
+		  }
+		  ArrayList<Element> children = list.get(i).getChildren();
+		  if (children != null) {
+			  if (moveToNewPosition(movingEntry, children)) return true;
+		  }
+	  }
+	  return false;
+  }
+  static private Element findEntryAtPositionRec(Element e, float x, float y) {
 	  if (isSelected(x, y, e.getLastBoundingBox())) return e;
-	  ArrayList<Entry> children = e.getChildren();
+	  ArrayList<Element> children = e.getChildren();
 	  if (children != null) {
-		  for (Entry child : children) {
-			  Entry e2 = findEntryAtPositionRec(child, x, y);
+		  for (Element child : children) {
+			  Element e2 = findEntryAtPositionRec(child, x, y);
 			  if (e2 != null) return e2;
 		  }
 	  }
 	  return null;
   }
+
+  private void tryFindMoveDestination(Element movingEntry, float x, float y, Repeats parent) {
+	  ArrayList<Element> children = (parent != null ? parent.getChildren() : mEntries);
+	  final int N = children.size();
+	  for (int i = 0; i < N; ++i) {
+		  final Element entry = children.get(i);
+		  if (entry instanceof PlaceholderDuringMove) continue;
+		  
+		  final RectF rect = entry.getLastBoundingBox();
+		  final float height = rect.bottom - rect.top;
+		  // If the point is in the upper 1/3 of the rect, create space above the entry.
+		  if (y < rect.top + height / 3 && y >= rect.top - 10) {
+			  PlaceholderDuringMove placeholder = getPlaceholder(movingEntry.getHeight());
+			  removeElement(placeholder);
+			  children.add(children.indexOf(entry), placeholder); 
+			  Log.d(TAG, "Move above: "+ entry.toString());
+			  return;
+		  }
+		  // If the point is in the bottom 1/3 of the rect, create space below the entry.
+		  if (y >= rect.bottom - height / 3 && y < rect.bottom- 10) {
+			  PlaceholderDuringMove placeholder = getPlaceholder(movingEntry.getHeight());
+			  removeElement(placeholder);
+			  children.add(children.indexOf(entry) + 1, placeholder); 
+			  Log.d(TAG, "Move below: "+ entry.toString());
+			  return;
+		  }
+	  }
+  }
+
+  private boolean removeElement(Element toRemove) {
+	  return removeElementRec(mEntries, toRemove);
+  }
+
+  private boolean replaceElement(Element toRemove, Element toAdd) {
+	  removeElement(toAdd);
+	  return replaceElementRec(mEntries, toRemove, toAdd);
+  }
   
-  private Entry findEntryAtPosition(float x, float y) {
-	  for (Entry entry : mEntries) {
-		  Entry e = findEntryAtPositionRec(entry, x, y);
+  private boolean replaceElementRec(ArrayList<Element> list, Element toRemove, Element toAdd) {
+	  final int N = list.size();
+	  for (int i = 0; i < N; ++i) {
+		  Element entry = list.get(i);
+		  if (entry == toRemove) {
+			  list.set(i, toAdd);
+			  return true;
+		  }
+		  ArrayList<Element> children = entry.getChildren();
+		  if (children != null) {
+			  if (replaceElementRec(children, toRemove, toAdd)) return true;
+		  }
+	  }
+	  return false;
+  }
+
+  private boolean removeElementRec(ArrayList<Element> list, Element toRemove) {
+	  final int N = list.size();
+	  for (int i = 0; i < N; ++i) {
+		  Element entry = list.get(i);
+		  if (entry == toRemove) {
+			  list.remove(i);
+			  return true;
+		  }
+		  ArrayList<Element> children = entry.getChildren();
+		  if (children != null) {
+			  if (removeElementRec(children, toRemove)) return true;
+		  }
+	  }
+	  return false;
+  }
+  
+  private Element findEntryAtPosition(float x, float y) {
+	  for (Element entry : mEntries) {
+		  Element e = findEntryAtPositionRec(entry, x, y);
 		  if (e != null) return e;
 	  }
 	  return null;
@@ -257,15 +391,12 @@ public class WorkoutCanvasView extends View implements View.OnTouchListener {
 	  final float density = getContext().getResources().getDisplayMetrics().scaledDensity;
 	  float x = 20 * density;
 	  
-	  for (Entry entry : mEntries) {
-		  if (entry == mMovingEntry) {
-			  float deltaX = mMoveStartX - mMoveCurrentX;
-			  float deltaY = mMoveStartY - mMoveCurrentY;			  
-			  entry.draw(canvas, x - deltaX, y - deltaY);
-		  } else {
-			  entry.draw(canvas, x, y);
-		  }
+	  for (Element entry : mEntries) {
+		  entry.draw(canvas, x, y);
 		  y += entry.getHeight();
+	  }
+	  if (mMovingEntry != null) {
+		  mMovingEntry.draw(canvas, mMoveCurrentX - mMoveDeltaX, mMoveCurrentY - mMoveDeltaY);
 	  }
   }
   
